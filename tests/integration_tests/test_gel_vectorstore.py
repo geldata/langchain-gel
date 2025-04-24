efeb8ece70b81026ed35e4c60025afc55e51a054
anbuzin <contactbuzin@gmail.com>
2025-04-24 16:28:46 +0700

    Update tests to support both IO modes

--- tests/integration_tests/test_gel_vectorstore.py
+++ tests/integration_tests/test_gel_vectorstore.py
@@ -11,7 +11,7 @@ try:
 except subprocess.CalledProcessError as e:
     print(e)
 
-class TestGelVectorStore(VectorStoreIntegrationTests):
+class TestGelVectorStoreSync(VectorStoreIntegrationTests):
     @pytest.fixture()
     def vectorstore(self) -> Generator[VectorStore, None, None]:  # type: ignore
         """Get an empty vectorstore for unit tests."""
@@ -26,3 +26,46 @@ class TestGelVectorStore(VectorStoreIntegrationTests):
             # cleanup operations, or deleting data
             store.delete()
 
+    @property
+    def has_sync(self) -> bool:
+        """
+        Configurable property to enable or disable sync tests.
+        """
+        return True
+
+    @property
+    def has_async(self) -> bool:
+        """
+        Configurable property to enable or disable async tests.
+        """
+        return False
+
+
+class TestGelVectorStoreAsync(VectorStoreIntegrationTests):
+    @pytest.fixture()
+    async def vectorstore(self) -> Generator[VectorStore, None, None]:  # type: ignore
+        """Get an empty vectorstore for unit tests."""
+
+        store = GelVectorStore(self.get_embeddings())
+        # note: store should be EMPTY at this point
+        # if you need to delete data, you may do so here
+        await store.adelete()
+        try:
+            yield store
+        finally:
+            # cleanup operations, or deleting data
+            await store.adelete()
+
+    @property
+    def has_sync(self) -> bool:
+        """
+        Configurable property to enable or disable sync tests.
+        """
+        return False
+
+    @property
+    def has_async(self) -> bool:
+        """
+        Configurable property to enable or disable async tests.
+        """
+        return True
